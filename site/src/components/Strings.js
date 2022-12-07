b6cb49a8b0a1ff12543607b885baf749163de742
Yu <ay3xqa@virginia.edu>
2022-12-07 17:49:24 -0500

    initial sketchfab inputs

---
+++ site/src/components/Strings.js
@@ -0,0 +1,6 @@
+const APP_CONSTANTS = {
+    HERO_MODEL : 'https://sketchfab.com/models/8d913bda48f84217902e6829982c494f/embed?ui_infos=0&amp;ui_watermark=0&amp;ui_help=0&amp;ui_settings=0&amp;ui_inspector=0&amp;ui_annotations=0&amp;ui_stop=0&amp;ui_vr=0&amp;preload=1&amp;autostart=1&amp;ui_hint=2&amp;autospin=0.2&amp;transparent=1'
+    , test : 'embed?ui_infos=0&amp;ui_watermark=0&amp;ui_help=0&amp;ui_settings=0&amp;ui_inspector=0&amp;ui_annotations=0&amp;ui_stop=0&amp;ui_vr=0&amp;preload=1&amp;autostart=1&amp;ui_hint=2&amp;autospin=0.2&amp;transparent=1'
+}
+
+export default APP_CONSTANTS;
